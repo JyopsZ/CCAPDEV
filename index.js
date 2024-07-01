6b67c4fdf4594ca2f3c9d523fc7c2298556a4e36
JyopsZ <timmyyap39@gmail.com>
2024-07-02 05:25:36 +0800

    Change hrefs v.1
    First checkpoint of href changes

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ var app = express();
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(express.static(__dirname));
 
-app.get('/', function(req, res) {
+app.get('/index', function(req, res) {
 	res.sendFile(__dirname + "\\" + "index.html");
 });
 
@@ -17,16 +17,46 @@ app.get('/register', function(req, res) {
 	res.sendFile(__dirname + "\\" + "register.html");
 });
 
+//Student studentPage
 app.get('/studentView/studentPage', function(req, res) {
-	res.sendFile(__dirname + "\\" + "studentPage.html");
+	res.sendFile(__dirname + "\\" + "/studentView/studentPage.html");
+});
+
+app.get('/studentView/view-availability', function(req, res) {
+	res.sendFile(__dirname + "\\" + "/studentView/view-availability.html");
 });
 
 app.get('/studentView/subReservation', function(req, res) {
-	res.sendFile(__dirname + "\\" + "subReservation.html");
+	res.sendFile(__dirname + "\\" + "/studentView/subReservation.html");
 });
 
 app.get('/studentView/subProfile', function(req, res) {
-	res.sendFile(__dirname + "\\" + "subProfile.html");
+	res.sendFile(__dirname + "\\" + "/studentView/subProfile.html");
+});
+
+//Student subReservation
+app.get('/studentView/reservation', function(req, res) {
+	res.sendFile(__dirname + "\\" + "/studentView/reservation.html");
+});
+
+app.get('/studentView/view-list-reservations', function(req, res) {
+	res.sendFile(__dirname + "\\" + "/studentView/view-list-reservations.html");
+});
+
+app.get('/studentView/edit-reservation', function(req, res) {
+	res.sendFile(__dirname + "\\" + "/studentView/edit-reservation.html");
+});
+
+
+
+// Lab Technician Labs
+app.get('/labtechView/labtechPage', function(req, res) {
+	res.sendFile(__dirname + "\\" + "labtechPage.html");
+});
+
+// Student Labs
+app.get('/studentView/studentPage', function(req, res) {
+	res.sendFile(__dirname + "\\" + "studentPage.html");
 });
 
 /*
