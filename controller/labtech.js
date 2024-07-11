cefaded8526fae80bde2c3c155df3a2daf98347f
Adrian Villador <116620032+diannnnnnnnnnnnnnnnnnnnnnn@users.noreply.github.com>
2024-07-11 18:25:20 +0800

    labtech

--- controller/labtech.js
+++ controller/labtech.js
@@ -1,82 +1,164 @@
 var express = require('express');
+const session = require("express-session");
 var router = express.Router();
 var path = require('path');
+const UserModel = require('../model/user');
 
-/**************************************** LAB TECHNICIAN ********************************************/
+
+// Middleware for checking if user is authenticated
+const isAuthenticated = (req, res, next) => {
+  if (req.session.user) {
+    next();
+  } else {
+    res.redirect("/login");
+  }
+};
 
 // Lab Tech main page
-router.get('/labtechView/labtechPage', function(req, res) {
-	res.sendFile(path.join(__dirname + "\\" + "../public/labtechView/labtechPage.html"));
+router.get('/labtechView/labtechPage', isAuthenticated, function(req, res) {
+  res.sendFile(path.join(__dirname, '/../public/labtechView/labtechPage.html'));
 });
 
-router.get('/labtechView/LViewAvailability', function(req, res) {
-	res.sendFile(path.join(__dirname + "\\" + "../public/labtechView/LViewAvailability.html"));
+router.get('/labtechView/LViewAvailability', isAuthenticated, function(req, res) {
+  res.sendFile(path.join(__dirname, '/../public/labtechView/LViewAvailability.html'));
 });
 
-router.get('/labtechView/LSubReservation', function(req, res) {
-	res.sendFile(path.join(__dirname + "\\" + "../public/labtechView/LSubReservation.html"));
+router.get('/labtechView/LSubReservation', isAuthenticated, function(req, res) {
+  res.sendFile(path.join(__dirname, '/../public/labtechView/LSubReservation.html'));
 });
 
-router.get('/labtechView/LSubProfile', function(req, res) {
-	res.sendFile(path.join(__dirname + "\\" + "../public/labtechView/LSubProfile.html"));
+router.get('/labtechView/LSubProfile', isAuthenticated, function(req, res) {
+  res.sendFile(path.join(__dirname, '/../public/labtechView/LSubProfile.html'));
 });
-
 
 // Lab Tech viewAvailability
-router.get('/labtechView/LReserveslot', function(req, res) {
-	res.sendFile(path.join(__dirname + "\\" + "../public/labtechView/LReserveslot.html"));
+router.get('/labtechView/LReserveslot', isAuthenticated, function(req, res) {
+  res.sendFile(path.join(__dirname, '/../public/labtechView/LReserveslot.html'));
 });
-
 
 // Lab Tech LSubReservation
-router.get('/labtechView/LReservation', function(req, res) {
-	res.sendFile(path.join(__dirname + "\\" + "../public/labtechView/LReservation.html"));
+router.get('/labtechView/LReservation', isAuthenticated, function(req, res) {
+  res.sendFile(path.join(__dirname, '/../public/labtechView/LReservation.html'));
 });
 
-router.get('/labtechView/LEditReservation', function(req, res) {
-	res.sendFile(path.join(__dirname + "\\" + "../public/labtechView/LEditReservation.html"));
+router.get('/labtechView/LEditReservation', isAuthenticated, function(req, res) {
+  res.sendFile(path.join(__dirname, '/../public/labtechView/LEditReservation.html'));
 });
 
-router.get('/labtechView/LRemoveReservationlist', function(req, res) {
-	res.sendFile(path.join(__dirname + "\\" + "../public/labtechView/LRemoveReservationlist.html"));
+router.get('/labtechView/LRemoveReservationlist', isAuthenticated, function(req, res) {
+  res.sendFile(path.join(__dirname, '/../public/labtechView/LRemoveReservationlist.html'));
 });
-
 
 // Lab Tech LReservation
-router.get('/labtechView/LRemoveReservationlist', function(req, res) {
-	res.sendFile(path.join(__dirname + "\\" + "../public/labtechView/LRemoveReservationlist.html"));
+router.get('/labtechView/LRemoveReservationlist', isAuthenticated, function(req, res) {
+  res.sendFile(path.join(__dirname, '/../public/labtechView/LRemoveReservationlist.html'));
 });
 
-router.get('/labtechView/lab1', function(req, res) {
-	res.sendFile(path.join(__dirname + "\\" + "../public/labtechView/lab1.html"));
+router.get('/labtechView/lab1', isAuthenticated, function(req, res) {
+  res.sendFile(path.join(__dirname, '/../public/labtechView/lab1.html'));
 });
 
-router.get('/labtechView/lab2', function(req, res) {
-	res.sendFile(path.join(__dirname + "\\" + "../public/labtechView/lab2.html"));
+router.get('/labtechView/lab2', isAuthenticated, function(req, res) {
+  res.sendFile(path.join(__dirname, '/../public/labtechView/lab2.html'));
 });
 
-router.get('/labtechView/lab3', function(req, res) {
-	res.sendFile(path.join(__dirname + "\\" + "../public/labtechView/lab3.html"));
+router.get('/labtechView/lab3', isAuthenticated, function(req, res) {
+  res.sendFile(path.join(__dirname, '/../public/labtechView/lab3.html'));
+});
+
+
+// Labtech Page
+router.get("/labtechPage", (req, res) => {
+  // Retrieve user data from the session
+  const user = req.session.user;
+  console.log(user);
+  res.render('labtechPage',{user});
 });
 
 
 // Lab Tech LSubProfile
-router.get('/labtechView/searchEditProfile', function(req, res) {
-	res.sendFile(path.join(__dirname + "\\" + "../public/labtechView/searchEditProfile.html"));
+router.get('/labtechView/searchEditProfile', isAuthenticated, function(req, res) {
+  res.sendFile(path.join(__dirname, '/../public/labtechView/searchEditProfile.html'));
 });
 
-router.get('/labtechView/LsearchOtherProfile', function(req, res) {
-	res.sendFile(path.join(__dirname + "\\" + "../public/labtechView/LsearchOtherProfile.html"));
+router.get('/labtechView/LsearchOtherProfile', isAuthenticated, function(req, res) {
+  res.sendFile(path.join(__dirname, '/../public/labtechView/LsearchOtherProfile.html'));
+});
+
+router.get('views/LsearchEditProfile', isAuthenticated, function(req, res) {
+  res.sendFile(path.join(__dirname, '/../public/labtechView/LsearchEditProfile'));
 });
 
 
-// Lab Tech LViewEditProfile
-router.get('/labtechView/LViewEditProfile', function(req, res) {
-	res.sendFile(path.join(__dirname + "\\" + "../public/labtechView/LViewEditProfile.html"));
+
+// Handling of form data to database
+router.post('/editInfoLabtech', isAuthenticated, async (req, res) => {
+  try {
+    const { firstName, lastName, password } = req.body;
+
+    // Find the user by userID
+    const userId = req.session.user.userID;
+    const user = await UserModel.findOne({ userID: userId });
+
+    if (!user) {
+      return res.status(404).send('User not found');
+    }
+
+    // Update the user's information
+    user.firstName = firstName;
+    user.lastName = lastName;
+    user.password = password;
+
+    // Save the updated user to the database
+    await user.save();
+
+    // Update the session data with the new user information
+    req.session.user = user;
+
+    res.redirect('/labtechView/LsearchEditProfile');
+  } catch (err) {
+    console.error(err);
+    res.status(500).send('Internal Server Error');
+  }
 });
 
-router.get('/labtechView/LViewOtherProfile', function(req, res) {
-	res.sendFile(path.join(__dirname + "\\" + "../public/labtechView/LViewOtherProfile.html"));
+//search for labtech
+router.post("/findUserLabtech", async (req, res) => {
+  try {
+      const { userName } = req.body;
+      const lowerCaseName = userName.toLowerCase();
+
+      let filter = {};
+
+      const names = lowerCaseName.trim().split(' ');
+      
+      if (names.length === 2) {
+          filter = {
+              $or: [
+                  { firstName: new RegExp(names[0], 'i'), lastName: new RegExp(names[1], 'i') },
+                  { firstName: new RegExp(names[1], 'i'), lastName: new RegExp(names[0], 'i') }
+              ]
+          };
+      } else if (names.length === 1) {
+          filter = {
+              $or: [
+                  { firstName: new RegExp(names[0], 'i') },
+                  { lastName: new RegExp(names[0], 'i') }
+              ]
+          };
+      }
+
+      console.log("Search filter:", filter);
+
+      const users = await UserModel.find(filter);
+
+      console.log("Found users:", users);
+
+      res.render('.../views/LViewEditProfile', { userData: users });
+  } catch (err) {
+      console.error(err);
+      res.status(500).send('Internal Server Error');
+  }
 });
 
 module.exports = router;
--- index.js
+++ index.js
@@ -49,6 +49,43 @@ app.use('/', labtechRoutes);
 var hbs = require('hbs')
 app.set('view engine','hbs');
 
+
+/* for searching and getting the data of the searched user */
+/*
+app.post("/findUserLab2", async (req, res) => {
+    const { userName } = req.body;
+    const lowerCaseUserName = userName.toLowerCase();
+    
+    try {
+        const users = await User.find(); // Retrieve all users from the database
+        const foundUser = users.find(user => 
+            `${user.firstName.toLowerCase()} ${user.lastName.toLowerCase()}` === lowerCaseUserName ||
+            user.firstName.toLowerCase() === lowerCaseUserName ||
+            user.lastName.toLowerCase() === lowerCaseUserName
+        );
+        if (foundUser) {
+            req.session.searchedUser = foundUser;  // Store found user in session
+            res.redirect("/LViewEditProfile");
+        } else {
+            res.send("User not found. <a href='/searchEditProfile'>Try again.</a>");
+        }
+    } catch (err) {
+        console.error('Error fetching user data:', err);
+        res.status(500).send('Internal Server Error');
+    }
+});
+
+// Route to render the profile view using session data
+app.get("/LViewEditProfile", (req, res) => {
+    const currUserLab = req.session.searchedUser;
+    if (currUserLab) {
+        res.render('LViewEditProfile', { userData: currUserLab });
+    } else {
+        res.send("No user selected. <a href='/searchEditProfile'>Search again.</a>");
+    }
+});
+*/
+
 var server = app.listen(3000, function() {
 	console.log("listening to port 3000...");
 });
